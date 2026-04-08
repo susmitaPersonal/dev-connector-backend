c8cc06a98bb5ba421b090f357db6e26ff808c544
Rohanhola1990 <amarnathds@gmail.com>
2026-04-08 15:48:34 +0530

    API of simple crud operations eith schema validation

--- src/app.js
+++ src/app.js
@@ -13,6 +13,64 @@ app.post("/signup", async (req, res) => {
     
 })
 
+app.get("/feed", async (req, res) => {
+    try{
+        console.log('Received request to /feed with body:', req.body);
+        if(!req.body) {
+            const allUsers = await User.find()
+            return res.status(200).json({data: allUsers, message: "Check out all the users in the database"})
+        }
+
+        const { emailId } = req.body;
+        console.log('Received emailId in request body:', emailId);
+        if(!emailId) {
+            return res.status(400).json({ error: 'Email ID is required' });
+
+        }
+        const userList = await User.find({ emailId})
+        if(!userList || userList.length === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        return res.status(201).json({ data: userList, message: "feed got fetched succesfully"})
+
+    } catch(err) {
+        console.log('Error fetching feed in app.js:', err);
+        res.status(500).json({ error: 'Failed to fetch feed' });
+    }
+})
+
+app.delete("/delete-user", async(req, res) => {
+    try {
+        const { _id } = req.params;
+        if(!_id) {
+            return res.status(400).json({ error: 'User ID is required' });
+        }
+        const deleteUser = await User.findByIdAndDelete(_id);
+        if(!deleteUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        return res.status(200).json({ data: deleteUser, message: 'User deleted successfully' });
+    } catch(err) {
+        console.log('Error deleting user in app.js:', err);
+        res.status(500).json({ error: 'Failed to delete user' });
+    }
+})
+
+app.patch("/update-user", async (req, res) => {
+    try {
+        const data = req.body;
+        if(!data || !data.id) {
+            return res.status(400).json({ error: 'User ID is required' });
+        }
+        const updateUser = await User.findByIdAndUpdate({
+            _id: data.id
+        }, data, { returnDocument: after, runValidators: true });
+    }
+    catch(err) {
+        return res.status(500).json({error: 'Failed to update user' });
+    }
+})
+
 // Connect to the database
 connectDB().then(() => {
     console.log('Database connection established in app.js');
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,67 @@
 const mongoose = require("mongoose");
+const { minLength, maxLength, uppercase } = require("zod");
 
 const UserSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 50
     },
     lastName: {
-        type: String
+        type: String,
+        minLength: 2,
+        maxLength: 50
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        uppercase: true,
+        trim: true
+    },
+     phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 10,
+        maxLength: 15
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        required: true,
+        min: 13,
+        max: 100
     },
     gender: {
-        type: String
+        type: String,
+        required: true,
+        validate(value){
+            if(!["Male", "female", "Others"].includes(value)) {
+                throw new Error("Gender must be either Male, Female, or Others")
+            }
+        }
+    },
+    about: {
+        type: string,
+        default: "This is the default about section. Please update it to tell us more about yourself!"
+    },
+    skills: {
+        type: [string]
+    },
+    photo_url: {
+        type: String,
+        default: "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-High-Quality-Image.png"
     }
+
+},
+{
+    timestamps: true
 })
 
 const UserModel = mongoose.model("User", UserSchema);
