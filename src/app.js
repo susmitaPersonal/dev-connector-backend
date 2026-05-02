2ee7cf8a0532656fd57f5e7c1c2dec54961838e5
susmitaPersonal <susmita8336.biswas@gmail.com>
2026-05-02 22:55:52 +0530

    add swagger json to import in postman - v2

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.get("/api-docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+});
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/', authRouter)
